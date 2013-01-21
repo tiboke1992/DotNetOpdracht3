4f4a662a503703a01f290deb9c9a801d384a41ad
tiboke1992 <tibo_hulens@hotmail.com>
2013-01-21 15:45:57 +0100

    Normaal werkt baseren op klas nu ook!

--- ASPNetBoekenApplicatie/ASPNetBoekenApplicatie/Default.aspx.cs
+++ ASPNetBoekenApplicatie/ASPNetBoekenApplicatie/Default.aspx.cs
@@ -15,26 +15,31 @@ namespace ASPNetBoekenApplicatie
 
         }
 
-        protected void categorie_dropdown_databound(Object sender, EventArgs e) {
+        protected void categorie_dropdown_databound(Object sender, EventArgs e)
+        {
             categorie_dropdown.Items.Insert(0, new ListItem("Alle Categorieen", "alle"));
         }
 
-        protected void uitgever_dropdown_databound(Object sender, EventArgs e) { 
+        protected void uitgever_dropdown_databound(Object sender, EventArgs e)
+        {
             uitgever_dropdown.Items.Insert(0, new ListItem("Alle uitgevers", "alle"));
         }
 
         protected void boeken_links_ids_Selecting(object sender, LinqDataSourceSelectEventArgs e)
         {
-            if (uitgever_dropdown.SelectedValue == "alle" && categorie_dropdown.SelectedValue == "alle") {
+            if (uitgever_dropdown.SelectedValue == "alle" && categorie_dropdown.SelectedValue == "alle")
+            {
                 boeken_links_ids.Where = @"titel.Contains(@titelParameter) and isbn.Contains(@isbnParameter)";
                 e.WhereParameters.Remove("categorieParameter");
                 e.WhereParameters.Remove("uitgeverParameter");
             }
-            else if (categorie_dropdown.SelectedValue == "alle") {
+            else if (categorie_dropdown.SelectedValue == "alle")
+            {
                 boeken_links_ids.Where = @"titel.Contains(@titelParameter) and isbn.Contains(@isbnParameter) and Uitgever.uitgeverID.Equals(@uitgeverParameter)";
                 e.WhereParameters.Remove("categorieParameter");
             }
-            else if (uitgever_dropdown.SelectedValue == "alle") {
+            else if (uitgever_dropdown.SelectedValue == "alle")
+            {
                 boeken_links_ids.Where = @"titel.Contains(@titelParameter) and isbn.Contains(@isbnParameter) and Categorie.CategorieID.Equals(@categorieParameter)";
                 e.WhereParameters.Remove("uitgeverParameter");
             }
@@ -47,20 +52,26 @@ namespace ASPNetBoekenApplicatie
 
             if (ControleVanVelden(klasNaam, aantalLeerlingen))
             {
+                int aantal = int.Parse(aantalLeerlingen);
                 createKlas(klasNaam, aantalLeerlingen);
-                gebaseerdOpKlas();
+                gebaseerdOpKlas(klasNaam, aantal);
+
             }
-            else {
+            else
+            {
                 showKlasErrorMessage();
             }
         }
 
-        private Boolean ControleVanVelden(String klasNaam, String aantalLeerlingen) {
+        private Boolean ControleVanVelden(String klasNaam, String aantalLeerlingen)
+        {
             Boolean result = false;
-            if (klasNaam != null && aantalLeerlingen != null && !klasNaam.Equals("") && !aantalLeerlingen.Equals("") && klasNaam.Length < 3) {
+            if (klasNaam != null && aantalLeerlingen != null && !klasNaam.Equals("") && !aantalLeerlingen.Equals("") && klasNaam.Length < 3)
+            {
                 //controle of klas al bestaat
                 IEnumerable<Boekenlijst> blist = dc.Boekenlijsts.Where(x => x.klas == klasNaam);
-                if (blist == null || blist.Count() == 0) {
+                if (blist == null || blist.Count() == 0)
+                {
                     int number;
                     Boolean isNumber = int.TryParse(aantalLeerlingen, out number);
                     if (isNumber && number > 0)
@@ -72,14 +83,17 @@ namespace ASPNetBoekenApplicatie
             return result;
         }
 
-        private void createKlas(String klasnaam, String aantalLeerlingen) {
+        private void createKlas(String klasnaam, String aantalLeerlingen)
+        {
             int aantal = int.Parse(aantalLeerlingen);
-            Boekenlijst boekenlijst = new Boekenlijst { 
+            Boekenlijst boekenlijst = new Boekenlijst
+            {
                 aantalLeerlingen = aantal,
                 klas = klasnaam,
                 statusID = 1,
                 laatstewijziging = DateTime.Now
             };
+
             dc.Boekenlijsts.InsertOnSubmit(boekenlijst);
             try
             {
@@ -89,12 +103,14 @@ namespace ASPNetBoekenApplicatie
                 ddlSelecteerKlas.DataBind();
                 ddlGebaseerd.DataBind();
             }
-            catch (LinqDataSourceValidationException e) {
+            catch (LinqDataSourceValidationException e)
+            {
                 lblError.Text = "Er was een probleem bij toevoegen van de klas!";
             }
         }
 
-        private void showKlasErrorMessage() {
+        private void showKlasErrorMessage()
+        {
             lblError.Text = "Er is een fout opget bij de controle van de gegevens mogelijk door: <br />";
             lblError.Text += "<ul>";
             lblError.Text += "<li>Naam of aantal leerlingen is niet ingevuld</li>";
@@ -106,31 +122,46 @@ namespace ASPNetBoekenApplicatie
             lblCorrect.Text = "";
         }
 
-        private void gebaseerdOpKlas() {
-            if (cbBasedOn.Checked == true) {
-                List<BoekBoekenlijst> onzeLijst = new List<BoekBoekenlijst>();
-                IEnumerable<BoekBoekenlijst> bblijst = dc.BoekBoekenlijsts.Where(x => x.klas == ddlGebaseerd.SelectedValue);
-                if (bblijst.Count() != 0) {
-                    foreach (BoekBoekenlijst bb in bblijst) {
-                        bb.klas = txtKlasNaam.Text;
-                        onzeLijst.Add(bb);
-                    }
+        private void gebaseerdOpKlas(String klasnaam, int aantalleerlingen)
+        {
+            if (cbBasedOn.Checked == true)
+            {
+                List<BoekBoekenlijst> onzelijst = new List<BoekBoekenlijst>();
+                IEnumerable<BoekBoekenlijst> hunLijst = dc.BoekBoekenlijsts.Where(x => x.klas == ddlGebaseerd.SelectedValue);
+                foreach (BoekBoekenlijst b in hunLijst)
+                {
+                    BoekBoekenlijst l = new BoekBoekenlijst();
+                    l.klas = txtKlasNaam.Text;
+                    l.id_boek = b.id_boek;
+                    l.huurprijs = b.huurprijs;
+                    l.schoolprijs = b.schoolprijs;
+                    l.wordtverhuurd = b.wordtverhuurd;
+                    l.categorieID = b.categorieID;
+                    onzelijst.Add(l);
                 }
-                dc.BoekBoekenlijsts.InsertAllOnSubmit(onzeLijst);
+                foreach (BoekBoekenlijst kk in onzelijst)
+                {
+                    dc.BoekBoekenlijsts.InsertOnSubmit(kk);
+                }
                 try
                 {
                     dc.SubmitChanges();
+                    lblError.Text = "";
+                    lblCorrect.Text = "Klas succesvol toegevoegd!";
+                    ddlSelecteerKlas.DataBind();
+                    ddlGebaseerd.DataBind();
                 }
                 catch (LinqDataSourceValidationException e)
                 {
-                    showKlasErrorMessage();
+                    lblError.Text = "Er was een probleem bij toevoegen van de klas!";
                 }
             }
         }
 
         protected void btnAddBoek_Click(object sender, EventArgs e)
         {
-            if (GridView1.SelectedValue != null) {
+            if (GridView1.SelectedValue != null)
+            {
                 int id = int.Parse(GridView1.SelectedValue.ToString());
                 IEnumerable<Boek> boek = dc.Boeks.Where(x => x.id == id);
                 IEnumerable<BoekBoekenlijst> ll = dc.BoekBoekenlijsts.Where(x => x.id_boek == id && x.klas == ddlSelecteerKlas.SelectedValue);
@@ -161,12 +192,13 @@ namespace ASPNetBoekenApplicatie
                         lblError.Text = "Fout bij toevoegen boek";
                     }
                 }
-            }     
+            }
         }
 
         protected void btnRemove_Click(object sender, EventArgs e)
         {
-            if (GridView2.SelectedValue != null) {
+            if (GridView2.SelectedValue != null)
+            {
                 int id = int.Parse(GridView2.SelectedValue.ToString());
                 IEnumerable<BoekBoekenlijst> bblijst = dc.BoekBoekenlijsts.Where(x => x.klas == ddlSelecteerKlas.SelectedValue && x.id_boek == id);
                 BoekBoekenlijst b = bblijst.First();
